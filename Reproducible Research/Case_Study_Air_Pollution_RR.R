8b782b2bd0faeaa2efdf85e2475d63ae7a8fe826
Marcelo <Marcelo>
2017-03-07 14:48:57 -0300

    Updated with RR course project 2

---
+++ Reproducible Research/Case_Study_Air_Pollution_RR.R
@@ -0,0 +1,11 @@
+# Air pollution = dust (metals, inert things like salt, and other components)
+# Air pollution Case Study - Reproducible Reserach
+
+# PM = Particulate matter
+# PM many different chemical elements
+# Some elements mey be more harmful, more dangerous
+# Identifying harmful chemical constituents lead to strategies 
+# for controlling sources of PM
+
+# CASE - The National Morbidity, Mortality and Air Pollution Study (NMMAPS)
+# short-term health effects of ambient air pollution
